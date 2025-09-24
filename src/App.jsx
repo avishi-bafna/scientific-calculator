564f13dd83972fa672b9a4213bd123249a1e5dc8
avishi-bafna <avishibafna@gmail.com>
2025-09-24 19:56:59 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Calculator from "./components/Calculator"
+
+function App() {
+
+  return (
+    <div className="App">
+      <Calculator />
+      <p className="developer">
+        Developed by 🧑‍💻 <span>Avishi Bafna</span>
+      </p>
+    </div>
+  )
+}
+
+export default App
